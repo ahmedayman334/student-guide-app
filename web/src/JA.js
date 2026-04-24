06dd7f783a85d9dba6f3858234079b6c9db1d344
Ahmed Khedr <ahmedkhedr018@gmail.com>
2026-04-24 14:08:24 +0300

    initial commit

--- web/src/JA.js
+++
@@ -1,9 +0,0 @@
-let btn=document.querySelector(".btn")
-let x=5;
-function GoToPage(){
-    if(x && x== 5)
-    {
-        window.location("/index.html")
-    }
-}
-btn.addEventListener("click",GoToPage)
